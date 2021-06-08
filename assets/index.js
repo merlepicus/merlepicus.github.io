874e0d76cb92f22553f9aa6824975a1a0df40e96
Daniel Trierweiler <danieltrierweiler@gmail.com>
2021-06-08 15:46:54 +0200

    Cleanup CSS + JS

--- assets/index.js
+++ assets/index.js
@@ -1,76 +1,73 @@
 var branding_height = 253;
 var scroll_offset = 10;
 
-
 //Change pos/background/padding/add shadow on nav when scroll event happens 
 $(function(){
 
-	// Add animation after domready to prevent jumping branding
-	$("#main-navigation").addClass("animated");
+    // Add animation after domready to prevent jumping branding
+    $("#main-navigation").addClass("animated");
 
+    // Remove animation and active class on intro-container when document is already scrolled
+    if ($(window).scrollTop() > scroll_offset) {
+        $("#intro-container").removeClass('active');
+        $("#main-navigation").removeClass("animated");
+    }
 
-	// Remove animation and active class on intro-container when document is already scrolled
-	if ($(window).scrollTop() > scroll_offset) {
-		$("#intro-container").removeClass('active');
-		$("#main-navigation").removeClass("animated");
-	}
+    // Toogle navigation and intro classes on scroll
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > scroll_offset) {
+            $("#intro-container").removeClass('active');
+            $("#burger-icon").removeClass('active');
+            $('#slogan').addClass('fixed');
 
-	// Toogle navigation and intro classes on scroll
-	$(window).scroll(function(){
-		if ($(window).scrollTop() > scroll_offset) {
-			$("#intro-container").removeClass('active');
-			$("#burger-icon").removeClass('active');
-			$('#slogan').addClass('fixed');
+        };
 
-		};
+        if ($(window).scrollTop() >= branding_height){
+            $('#main-navigation').addClass('fixed');
+            $('#burger-icon').addClass('fixed');
+        } else {
+            $('#main-navigation').removeClass('fixed');
 
-		if ($(window).scrollTop() >= branding_height){
-			$('#main-navigation').addClass('fixed');
-			$('#burger-icon').addClass('fixed');
-		} else {
-			$('#main-navigation').removeClass('fixed');
+        }
+    });
 
-		}
-	});
+    if ($(window).scrollTop() > branding_height) {
+        $('#main-navigation').addClass('fixed');
+        $('#burger-icon').addClass('fixed');
+    }
+    
+    $("#chevron").on("click", function(){
+        $("#intro-container").removeClass('active');
+        $("#burger-icon").removeClass('active');
+        $('#slogan').addClass('fixed');
+    });
 
-	if ($(window).scrollTop() > branding_height) {
-		$('#main-navigation').addClass('fixed');
-		$('#burger-icon').addClass('fixed');
-	}
-	
-	$("#chevron").on("click", function(){
-		$("#intro-container").removeClass('active');
-		$("#burger-icon").removeClass('active');
-		$('#slogan').addClass('fixed');
-	});
+    // Splide Slider
+    var splide = new Splide('.splide', {
+        focus: 'center',
+        type: 'loop',
+        perPage: 3,
+        gap: '1rem',
+        height: '18rem',
+        cover: true,
+        breakpoints: {
+            '767': {
+                perPage: 1,
+            },
+        }
+    }).mount();
 
-	// Splide Slider
-	var splide = new Splide('.splide', {
-	  focus: 'center',
-	  type: 'loop',
-	  perPage: 3,
-	  gap: '1rem',
-	  height: '18rem',
-		cover: true,
-	  breakpoints: {
-			'767': {
-				perPage: 1,
-			},
-		}
-	}).mount();
-
-	splide.on('move', function(newIndex, oldIndex, destIndex) {
-	  $('.splide__list').find(".slide-description").addClass("hidden");
-	  $('.splide__slide').find('.desc-' + newIndex).removeClass("hidden");
-	});
-
+    splide.on('move', function(newIndex, oldIndex, destIndex) {
+        $('.splide__list').find(".slide-description").addClass("hidden");
+        $('.splide__slide').find('.desc-' + newIndex).removeClass("hidden");
+    });
 });
 
 // Mobile Navigation
 function toggleNav() {
-	if (document.getElementById("mobile-nav").style.width == "100%") {
-		document.getElementById("mobile-nav").style.width = "0%";
-	} else {
-		document.getElementById("mobile-nav").style.width = "100%";
-	}
+    if (document.getElementById("mobile-nav").style.width == "100%") {
+        document.getElementById("mobile-nav").style.width = "0%";
+    } else {
+        document.getElementById("mobile-nav").style.width = "100%";
+    }
 }
