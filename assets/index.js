6b80ac5bd4761d45a2d7283dc2803385251563c8
Daniel Trierweiler <danieltrierweiler@gmail.com>
2021-06-08 14:56:37 +0200

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -73,9 +73,4 @@ function toggleNav() {
 	} else {
 		document.getElementById("mobile-nav").style.width = "100%";
 	}
-	
-}
-
-function closeNav() {
-	document.getElementById("mobile-nav").style.width = "0%";
 }
