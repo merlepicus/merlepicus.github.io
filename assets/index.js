39c4630cdb7493fc100c9c2597c70ffa9ba1a010
MerlePicus <merle.specht@evlka.de>
2021-06-08 11:58:31 +0200

    Navi change

--- assets/index.js
+++ assets/index.js
@@ -59,6 +59,114 @@ $(function(){
 
 });
 
+// Navigation Change on Scroll
+$('#jubilaeum').addClass("active");
+
+/* Smooth scrolling*/
+$("#s1").click(function() {
+     $('html, body').animate({
+         scrollTop:        $("#jubilaeum").offset().top-65
+     }, 1000);
+  return false;
+ });
+$("#s2").click(function() {
+     $('html, body').animate({
+         scrollTop:        $("#chronologie").offset().top-100
+     }, 1000);
+  return false;
+ });
+
+$("#s3").click(function() {
+     $('html, body').animate({
+         scrollTop:        $("#leistungen").offset().top-100
+     }, 1000);
+  return false;
+ });
+
+$("#s4").click(function() {
+  $(this).addClass("active");
+     $('html, body').animate({
+         scrollTop:        $("#massschuhe").offset().top-100
+     }, 1000);
+  return false;
+ });
+ $("#s5").click(function() {
+	$(this).addClass("active");
+	   $('html, body').animate({
+		   scrollTop:        $("#pflegetipps").offset().top-100
+	   }, 1000);
+	return false;
+   });
+$("#s6").click(function() {
+$(this).addClass("active");
+	$('html, body').animate({
+		scrollTop:        $("#stimmen").offset().top-100
+	}, 1000);
+return false;
+});
+
+$("#toTop").click(function() {
+     $('html, body').animate({
+         scrollTop:        $("#1").offset().top-65
+     }, 1000);
+  return false;
+ });
+/*$('#2').waypoint(function(event, direction) {
+
+  $(".nav-container ul li").children().removeClass("active");
+  $("#s2").addClass("active");
+  
+  if (direction === 'down') {
+    offset = 90;
+  } 
+  else {
+    offset = 20;
+  }
+});*/
+
+
+/*
+Using jquery waypoints to change active on scroll
+*/
+$('#chronologie').waypoint(function() {
+
+  $(".nav-container ul li").children().removeClass("active");
+  $("#s2").addClass("active");
+  
+}, { offset: 101 });
+
+
+$('#leistungen').waypoint(function() {
+  $(".nav-container ul li").children().removeClass("active");
+  $("#s3").addClass("active");
+}, { offset: 101 });
+
+$('#massschuhe').waypoint(function() {
+  $(".nav-container ul li").children().removeClass("active");
+  $("#s4").addClass("active");
+}, { offset: 101 });
+
+$('#pflegetipps').waypoint(function() {
+  $(".nav-container ul li").children().removeClass("active");
+  $("#s1").addClass("active");
+}, { offset: 0 });
+
+$('#stimmen').waypoint(function() {
+	$(".nav-container ul li").children().removeClass("active");
+	$("#s1").addClass("active");
+  }, { offset: 0 });
+
+  
+$('#chronologie').waypoint(function() {
+  $(".to-top").addClass("visible");
+}, { offset: 100 });
+
+$('#massschuhe').waypoint(function(event, direction) {
+  $(".to-top").removeClass("visible");
+}, { offset: 30 });
+
+
+
 // Mobile Navigation
 function openNav() {
 	document.getElementById("mobile-nav").style.width = "100%";
